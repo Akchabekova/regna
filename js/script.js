d0b00c8bf53037373f7e99adb70f9dea5dddac3a
Akchabekova <Luelum7@gmail.com>
2022-01-09 23:17:19 +0300

    finished burger menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
-const myBurger =document.querySelector('.burger')
-const myMenu =document.querySelector('.menu')
+const myBurger = document.querySelector('.burger')
+const myMenu = document.querySelector('.menu')
+const menuItem = document.querySelectorAll('.menu-item')
+
+
 
 myBurger.addEventListener('click', () => {
     myMenu.classList.toggle('show')
+    document.body.classList.toogle('o-hidden')
+})
+
+menuItem.forEach(onelink => {
+    onelink.addEventListener('click', () => {
+        myMenu.classList.remove('show')
+        document.body.classList.remove('o-hidden')
+    })
+
 })
